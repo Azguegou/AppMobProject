69ea9f95fdb7b008dede6e3853fd40b568f49e86
ickx <mizoudaaa@gmail.com>
2023-06-28 00:52:30 +0200

    ADD : tour du joueur dans l'algo, vérification et restart si la suite n'est pas bonne

--- app/src/main/java/com/example/tp1a/SimonActivity.kt
+++ app/src/main/java/com/example/tp1a/SimonActivity.kt
@@ -22,6 +22,11 @@ class SimonActivity : AppCompatActivity() {
     private lateinit var btnJouer: Button
     private lateinit var btnDeco: Button
 
+    private val memoryList: ArrayList<Int> = ArrayList()
+    private val playerList: ArrayList<Int> = ArrayList()
+
+    private var nbTours : Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
@@ -35,6 +40,13 @@ class SimonActivity : AppCompatActivity() {
         btnJouer = findViewById(R.id.buttonJouer)
         btnDeco = findViewById(R.id.buttonDeco)
 
+        // Dans la fonction onCreate()
+        btn1.isEnabled = false
+        btn2.isEnabled = false
+        btn3.isEnabled = false
+        btn4.isEnabled = false
+
+
         btnJouer.setOnClickListener() {
             party()
         }
@@ -46,30 +58,35 @@ class SimonActivity : AppCompatActivity() {
 
     fun party() {
         val nbCarrees = 4
-        var nbTours = 5
+        nbTours += 1
 
-        // Liste du jeu
-        val memoryList = ArrayList<Int>()
+        // Effacer la liste du joueur pour la prochaine manche
+        playerList.clear()
+
 
         // Générez les valeurs aléatoires dans memoryList
-        //TODO : enlever cette boucle for à la fin du projet
-        for (i in 1..nbTours) {
-            memoryList.add(Random.nextInt(1, nbCarrees + 1))
-        }
+        memoryList.add(Random.nextInt(1, nbCarrees + 1))
 
-        Toast.makeText(
-            this@SimonActivity,
-            "Retenez cette suite",
-            Toast.LENGTH_SHORT
-        ).show()
+
+        //Toast.makeText(
+        //    this@SimonActivity,
+        //    "Retenez cette suite",
+        //    Toast.LENGTH_SHORT
+        //).show()
 
         clignoterBoutons(memoryList)
 
-        Toast.makeText(
-            this@SimonActivity,
-            "à vous de jouer !",
-            Toast.LENGTH_SHORT
-        ).show()
+        Handler(Looper.getMainLooper()).postDelayed({
+            Toast.makeText(
+                this@SimonActivity,
+                "À vous de jouer !",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            enableButtonInput()
+        }, (1500L))
+
+
     }
 
 
@@ -93,10 +110,62 @@ class SimonActivity : AppCompatActivity() {
                 button?.let {
                     it.setBackgroundColor(Color.WHITE)
                     handler.postDelayed({
-                        it.setBackgroundColor(Color.parseColor("#B1B2FF"));
+                        it.setBackgroundColor(Color.parseColor("#B1B2FF"))
+
                     }, delay/2)
                 }
             }, (i * delay) + delay)
         }
     }
+    private fun enableButtonInput() {
+        btn1.setOnClickListener {
+            addToPlayerList(1)
+        }
+        btn2.setOnClickListener {
+            addToPlayerList(2)
+        }
+        btn3.setOnClickListener {
+            addToPlayerList(3)
+        }
+        btn4.setOnClickListener {
+            addToPlayerList(4)
+        }
+
+        // Activer les boutons
+        btn1.isEnabled = true
+        btn2.isEnabled = true
+        btn3.isEnabled = true
+        btn4.isEnabled = true
+    }
+
+    private fun addToPlayerList(buttonIndex: Int) {
+        playerList.add(buttonIndex)
+        checkPlayerList()
+
+    }
+
+    private fun checkPlayerList() {
+        var isCorrect = true
+        for (i in playerList.indices) {
+            if (playerList[i] != memoryList[i]) {
+                isCorrect = false
+                break
+            }
+        }
+        if (!isCorrect) {
+            //suite incorrecte
+            Toast.makeText(
+                this@SimonActivity,
+                "Désolé, la séquence est incorrecte.",
+                Toast.LENGTH_SHORT
+            ).show()
+            memoryList.clear()
+            playerList.clear()
+        } else if (playerList.size == memoryList.size) {
+            //suite correte
+            party()
+            playerList.clear()
+        }
+    }
+
 }
