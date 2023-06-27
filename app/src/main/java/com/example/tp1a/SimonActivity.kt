2f6e06a84deaa13255b4c1e41cee19614d404528
ickx <mizoudaaa@gmail.com>
2023-06-27 18:11:34 +0200

    Algo à jour (func party()) mais appli crash

--- app/src/main/java/com/example/tp1a/SimonActivity.kt
+++ app/src/main/java/com/example/tp1a/SimonActivity.kt
@@ -14,33 +14,7 @@ class SimonActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.simon)
-
-        party()
-
     }
 
-    fun party() {
-        val nbCarrees = 4
-        var nbTours = 0
-
-        //variables locales des boutons du Simon
-        val btn1 = findViewById<Button>(R.id.simonButton1)
-        val btn2 = findViewById<Button>(R.id.simonButton2)
-        val btn3 = findViewById<Button>(R.id.simonButton3)
-        val btn4 = findViewById<Button>(R.id.simonButton4)
-        val username = findViewById<TextView>(R.id.playerName)
-        val score = findViewById<TextView>(R.id.score)
-
-        username.setText(intent.getStringExtra("username"))
-        score.setText(0)
-
-        //liste du jeu
-        val memoryList = ArrayList<Int>()
-
-        //liste du joueur, à comparer avec la liste du Jeu
-        val playerList = ArrayList<Int>()
-
-
-    }
 }
 
