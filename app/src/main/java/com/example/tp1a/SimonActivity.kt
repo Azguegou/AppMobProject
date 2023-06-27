c6ab30edd516c93138788929ec1f280f41de86b3
ickx <mizoudaaa@gmail.com>
2023-06-27 21:34:51 +0200

    add : clignotement des boutons à l'ouverture de la page Simon

--- app/src/main/java/com/example/tp1a/SimonActivity.kt
+++ app/src/main/java/com/example/tp1a/SimonActivity.kt
@@ -2,6 +2,8 @@ package com.example.tp1a
 
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,87 +12,66 @@ import kotlin.random.Random
 
 class SimonActivity : AppCompatActivity() {
 
+    // Déclarez ces variables en tant que propriétés de classe
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
+    private lateinit var btn3: Button
+    private lateinit var btn4: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.simon)
+
+        // Initialisez les boutons
+        btn1 = findViewById(R.id.simonButton1)
+        btn2 = findViewById(R.id.simonButton2)
+        btn3 = findViewById(R.id.simonButton3)
+        btn4 = findViewById(R.id.simonButton4)
+
+        party() // Appelez la fonction party() pour démarrer le clignotement des boutons
     }
 
     fun party() {
         val nbCarrees = 4
-        var nbTours = 0
+        var nbTours = 12
 
-        //variables locales des boutons du Simon
-        val btn1 = findViewById<Button>(R.id.simonButton1)
-        val btn2 = findViewById<Button>(R.id.simonButton2)
-        val btn3 = findViewById<Button>(R.id.simonButton3)
-        val btn4 = findViewById<Button>(R.id.simonButton4)
-        val username = findViewById<TextView>(R.id.playerName)
-        val score = findViewById<TextView>(R.id.score)
-
-        //username.text = intent.(username)
-
-        //liste du jeu
+        // Liste du jeu
         val memoryList = ArrayList<Int>()
 
-        //liste du joueur, à comparer avec la liste du Jeu
-        val playerList = ArrayList<Int>()
+        // Générez les valeurs aléatoires dans memoryList
+        for (i in 1..nbTours) {
+            memoryList.add(Random.nextInt(1, nbCarrees + 1))
+        }
+        
+        clignoterBoutons(memoryList)
+    }
 
-        //Algo d'une partie
-        while(playerList == memoryList) {
-            //remise à 0 de la liste du joueur
-            for (e in playerList)
-                playerList.removeAt(e)
 
-            // choix aléatoire d'un carré
-            val simonRandomVal = Random.nextInt(1, nbCarrees + 1)
-            memoryList.add(simonRandomVal)
-            nbTours++
 
-            for (e in memoryList) {
-                if (e==1) {
-                    btn1.setBackgroundColor(Color.RED)
-                    Thread.sleep(1000)
-                    btn1.setBackgroundResource(R.drawable.border)
-                    Thread.sleep(500)
-                }
-                else if (e==2) {
-                    btn2.setBackgroundColor(Color.RED)
-                    Thread.sleep(1000)
-                    btn1.setBackgroundResource(R.drawable.border)
-                    Thread.sleep(500)
-                }
-                else if (e==3) {
-                    btn3.setBackgroundColor(Color.RED)
-                    Thread.sleep(1000)
-                    btn1.setBackgroundResource(R.drawable.border)
-                    Thread.sleep(500)
-                }
-                else {
-                    btn4.setBackgroundColor(Color.RED)
-                    Thread.sleep(1000)
-                    btn1.setBackgroundResource(R.drawable.border)
-                    Thread.sleep(500)
-                }
+    fun clignoterBoutons(memoryList: ArrayList<Int>) {
+        val delay = 1000L // délai de 1000ms
+        val handler = Handler(Looper.getMainLooper())
+
+        for (i in memoryList.indices) {
+
+            //selection des bouttons en fonction de l'indice dans la liste
+            val button = when (memoryList[i]) {
+                1 -> btn1
+                2 -> btn2
+                3 -> btn3
+                4 -> btn4
+                else -> null
             }
 
-            for (i in 0..nbTours) {
-                btn1.setOnClickListener() {
-                    playerList.add(1)
+            handler.postDelayed({
+                button?.let {
+                    it.setBackgroundColor(Color.WHITE)
+                    handler.postDelayed({
+                        it.setBackgroundColor(Color.parseColor("#B1B2FF"));
+                    }, delay/2)
                 }
-                btn2.setOnClickListener() {
-                    playerList.add(2)
-                }
-                btn3.setOnClickListener() {
-                    playerList.add(3)
-                }
-                btn4.setOnClickListener() {
-                    playerList.add(4)
-                }
-                if (playerList[i] != memoryList[i])
-                    break
-            }
+            }, (i * delay) + delay)
         }
     }
 }
-
