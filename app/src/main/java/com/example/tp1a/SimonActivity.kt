0ce81464be2b7f80f4e691a98fee3bf0c8ea9ff4
ickx <mizoudaaa@gmail.com>
2023-06-27 17:23:36 +0200

    intermediaire

--- app/src/main/java/com/example/tp1a/SimonActivity.kt
+++ app/src/main/java/com/example/tp1a/SimonActivity.kt
@@ -31,7 +31,8 @@ class SimonActivity : AppCompatActivity() {
         val username = findViewById<TextView>(R.id.playerName)
         val score = findViewById<TextView>(R.id.score)
 
-        username.text = intent.(username)
+        username.setText(intent.getStringExtra("username"))
+        score.setText(0)
 
         //liste du jeu
         val memoryList = ArrayList<Int>()
@@ -39,61 +40,7 @@ class SimonActivity : AppCompatActivity() {
         //liste du joueur, à comparer avec la liste du Jeu
         val playerList = ArrayList<Int>()
 
-        //Algo d'une partie
-        while(playerList == memoryList) {
-            //remise à 0 de la liste du joueur
-            for (e in playerList)
-                playerList.removeAt(e)
 
-            // choix aléatoire d'un carré
-            val simonRandomVal = Random.nextInt(1, nbCarrees + 1)
-            memoryList.add(simonRandomVal)
-            nbTours++
-
-            for (e in memoryList) {
-                if (e==1) {
-                    btn1.setBackgroundColor(Color.RED)
-                    Thread.sleep(1000)
-                    btn1.setBackgroundResource(R.drawable.border)
-                    Thread.sleep(500)
-                }
-                else if (e==2) {
-                    btn2.setBackgroundColor(Color.RED)
-                    Thread.sleep(1000)
-                    btn1.setBackgroundResource(R.drawable.border)
-                    Thread.sleep(500)
-                }
-                else if (e==3) {
-                    btn3.setBackgroundColor(Color.RED)
-                    Thread.sleep(1000)
-                    btn1.setBackgroundResource(R.drawable.border)
-                    Thread.sleep(500)
-                }
-                else {
-                    btn4.setBackgroundColor(Color.RED)
-                    Thread.sleep(1000)
-                    btn1.setBackgroundResource(R.drawable.border)
-                    Thread.sleep(500)
-                }
-            }
-
-            for (i in 0..nbTours) {
-                btn1.setOnClickListener() {
-                    playerList.add(1)
-                }
-                btn2.setOnClickListener() {
-                    playerList.add(2)
-                }
-                btn3.setOnClickListener() {
-                    playerList.add(3)
-                }
-                btn4.setOnClickListener() {
-                    playerList.add(4)
-                }
-                if (playerList[i] != memoryList[i])
-                    break
-            }
-        }
     }
 }
 
